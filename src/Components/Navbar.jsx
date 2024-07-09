5c598ac0dfc6865cee06dd14f8993da826e832fa
mdaminul1024 <mdfahad1024@gmail.com>
2024-07-09 17:20:27 +0600

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Navbar(){
     return (
         <nav className="flex bg-color justify-between items-center bg-black px-8 py-3">
-            <div className="text-xl font-medium text-white"><Link href={"/"}>GTCoding</Link></div>
-            <div className="bg-white py-2 px-4 hover:bg-slate-50 duration-200"><Link href={"/add-topic"}>Add Topic</Link></div>
+            <div className="first text-xl font-medium text-white"><Link href={"/"}>GTCoding</Link></div>
+            <div className="second bg-white py-2 px-4 hover:bg-slate-50 duration-200"><Link href={"/add-topic"}>Add Topic</Link></div>
         </nav>
     )
 }
